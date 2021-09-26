cb85acaa1d45f92517e25e94ca465d7310a4daa2
sungho <leski@naver.com>
2021-09-27 00:13:35 +0900

    first commit

---
+++ src/main/java/com/s1c/rtp/RtpApplication.java
@@ -0,0 +1,13 @@
+package com.s1c.rtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtpApplication.class, args);
+	}
+
+}
