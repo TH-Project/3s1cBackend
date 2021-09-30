b49dc077cc5f746a06534ee4b0287b26d0b8dcd9
LuneRemer <europejumin@gmail.com>
2021-09-30 17:23:00 +0900

    NewsController added

---
+++ src/main/java/com/s1c/rtp/controller/NewsController.java
@@ -0,0 +1,18 @@
+package com.s1c.rtp.controller;
+
+import java.util.List;
+
+import com.s1c.rtp.dto.Test2dto;
+import com.s1c.rtp.entity.TestEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.s1c.rtp.service.TestService;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api")
+public class NewsController {
+}
