38e6c3b8ba99410fdfbe616b0f6648696ef75ada
LuneRemer <europejumin@gmail.com>
2021-09-30 17:35:40 +0900

    Controller classes added

---
+++ src/main/java/com/s1c/rtp/controller/AgeController.java
@@ -0,0 +1,19 @@
+package com.s1c.rtp.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.s1c.rtp.entity.ageanalysis;
+import com.s1c.rtp.repository.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api")
+public class AgeController {
+}
