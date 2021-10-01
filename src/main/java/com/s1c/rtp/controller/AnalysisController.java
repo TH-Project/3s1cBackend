e48ef1085edeb6d9dcec9ad1270d896abae04ba8
LuneRemer <europejumin@gmail.com>
2021-10-02 02:38:33 +0900

    Project Structure changed depending on API spec

---
+++ src/main/java/com/s1c/rtp/controller/AnalysisController.java
@@ -0,0 +1,38 @@
+package com.s1c.rtp.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.s1c.rtp.entity.genderanalysis;
+import com.s1c.rtp.entity.news;
+import com.s1c.rtp.repository.NewsRepository;
+import com.s1c.rtp.repository.GenderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api")
+public class AnalysisController {
+
+    @Autowired
+    NewsRepository newsRepository;
+
+    @Autowired
+    GenderRepository genderRepository;
+
+    @GetMapping("/real-time-popularity/news/all")
+    public List<news> retrieveAllNews(){
+        List<news> result = newsRepository.findAll();
+        return result;
+    }
+
+    @GetMapping("/real-time-popularity/gender/all")
+    public List<genderanalysis> retrieveAllGender(){
+        List<genderanalysis> result = genderRepository.findAll();
+        return result;
+    }
+}
