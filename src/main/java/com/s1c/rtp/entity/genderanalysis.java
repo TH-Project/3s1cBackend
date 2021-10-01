5f4ab19195ef424a093fe611e94e4d7d1781a6fd
LuneRemer <europejumin@gmail.com>
2021-10-02 02:36:50 +0900

    Project Structure changed depending on API spec

--- src/main/java/com/s1c/rtp/entity/genderanalysis.java
+++ src/main/java/com/s1c/rtp/entity/genderanalysis.java
@@ -1,5 +1,6 @@
 package com.s1c.rtp.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -15,6 +16,7 @@ public class genderanalysis {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "gender_id")
     private int genderId;
+
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "news_id")
