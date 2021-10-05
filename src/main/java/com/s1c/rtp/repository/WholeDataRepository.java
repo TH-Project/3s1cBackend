3dc5ecd44c101ea2a8d8323e6f3b42adc5743aa9
LuneRemer <europejumin@gmail.com>
2021-10-05 21:24:36 +0900

    Added WholeDataService class

--- src/main/java/com/s1c/rtp/repository/WholeDataRepository.java
+++ src/main/java/com/s1c/rtp/repository/WholeDataRepository.java
@@ -2,12 +2,29 @@ package com.s1c.rtp.repository;
 
 import com.s1c.rtp.entity.WholeDataNum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 public interface WholeDataRepository extends JpaRepository<WholeDataNum, Timestamp> {
 
+    @Modifying
+    @Query("update WholeDataNum w set w.today = current_timestamp")
+    void updateToday();
+
+    @Query("select count(n) from news n")
+    Long numOfNewsData();
+
+    @Query("select count(c) from comments c")
+    Long numOfCommentsData();
+
+    @Modifying
+    @Query("update WholeDataNum w set w.today_comments = :comments")
+    void updateNumOfComments(@Param("comments") Long comments);
+
+    @Modifying
+    @Query("update WholeDataNum w set w.today_news = :news")
+    void updateNumOfNews(@Param("news") Long news);
 }
---
+++ src/main/java/com/s1c/rtp/service/WholeDataService.java
@@ -0,0 +1,31 @@
+package com.s1c.rtp.service;
+
+import com.s1c.rtp.dto.WholeDataDto;
+import com.s1c.rtp.entity.WholeDataNum;
+import com.s1c.rtp.repository.WholeDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.s1c.rtp.repository.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class WholeDataService {
+
+    @Autowired
+    WholeDataRepository wholeDataRepository;
+
+    @Transactional
+    public void updateWholeDataTable(){
+        Long comments_today = wholeDataRepository.numOfCommentsData();
+        Long news_today = wholeDataRepository.numOfNewsData();
+
+        wholeDataRepository.updateNumOfComments(comments_today);
+        wholeDataRepository.updateNumOfNews(news_today);
+
+        wholeDataRepository.updateToday();
+    }
+}
