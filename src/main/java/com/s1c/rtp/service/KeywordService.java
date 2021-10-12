f7486b872e360547b514bc7455bd09b8316a5305
LuneRemer <europejumin@gmail.com>
2021-10-13 01:11:51 +0900

    Add keywordService class

---
+++ src/main/java/com/s1c/rtp/service/KeywordService.java
@@ -0,0 +1,16 @@
+package com.s1c.rtp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.s1c.rtp.repository.*;
+import com.s1c.rtp.dto.*;
+import com.s1c.rtp.entity.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class KeywordService {
+}
