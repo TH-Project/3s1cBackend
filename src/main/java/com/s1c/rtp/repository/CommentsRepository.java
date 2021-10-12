a9775953149afa4b1e36c7fe8d245a26c0e68b99
LuneRemer <europejumin@gmail.com>
2021-10-13 04:30:08 +0900

    Add API returns TimeGraph Method & Query

--- src/main/java/com/s1c/rtp/repository/CommentsRepository.java
+++ src/main/java/com/s1c/rtp/repository/CommentsRepository.java
@@ -3,20 +3,25 @@ package com.s1c.rtp.repository;
 
 import com.s1c.rtp.dto.CommentsDto;
 import com.s1c.rtp.dto.GroupByDto;
+import com.s1c.rtp.dto.TimeCommentsDto;
 import com.s1c.rtp.entity.comments;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface CommentsRepository extends JpaRepository<comments, Integer>{
 
-    @Query("select new com.s1c.rtp.dto.CommentsDto(c.commentsId, n.title, c.contents, c.writer) from comments c join c.news_comments n where c.commentsId < 3")
+    @Query("select new com.s1c.rtp.dto.CommentsDto(c.commentsId, n.title, c.contents, c.writer, c.date) from comments c join c.news_comments n where c.commentsId < 3")
     List<CommentsDto> findCommentsWithNews();
 
     @Query("select count(distinct c.writer) from comments c")
     Long returnNumOfWriters();
+
+    @Query("select new com.s1c.rtp.dto.TimeCommentsDto(c.commentsId, c.date) from comments c")
+    List<TimeCommentsDto> findAllCommentsNTime();
 
 //    @Query("select new com.s1c.rtp.dto.GroupByDto(:numOfComments, :numOfWriters) from comments c")
 //    List<GroupByDto> returnWritersNComments(@Param("numOfComments") Long numOfComments, @Param("numOfWriters") Long numOfWriters);
--- src/main/java/com/s1c/rtp/service/CommentsService.java
+++ src/main/java/com/s1c/rtp/service/CommentsService.java
@@ -1,5 +1,7 @@
 package com.s1c.rtp.service;
 
+import com.mysql.cj.xdevapi.JsonArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,11 +23,104 @@ public class CommentsService {
     WholeDataRepository wholeDataRepository;
 
     @Transactional
-    public GroupByDto returnNumOfWritersNComments(){
+    public GroupByDto returnNumOfWritersNComments() {
         Long numOfComments = wholeDataRepository.numOfCommentsData();
         Long numOfWriters = commentsRepository.returnNumOfWriters();
 
         return new GroupByDto(numOfComments, numOfWriters);
     }
 
+    @Transactional
+    public JSONObject returnCommentsNTime() {
+        int zero, one, two, three, four, five, six, seven, eight, nine, ten;
+        int eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen, twenty;
+        int twentyOne, twentyTwo, twentyThree;
+
+        zero = one = two = three = four = five = six = seven = eight = nine = ten = 0;
+        eleven = twelve = thirteen = fourteen = fifteen = sixteen = seventeen = eighteen = nineteen = twenty = 0;
+        twentyOne = twentyTwo = twentyThree = 0;
+
+        List<TimeCommentsDto> timeCommentsDtoList = commentsRepository.findAllCommentsNTime();
+        for (TimeCommentsDto obj : timeCommentsDtoList) {
+            if (obj.getTime() == 0) {
+                zero++;
+            } else if (obj.getTime() == 1) {
+                one++;
+            } else if (obj.getTime() == 2) {
+                two++;
+            } else if (obj.getTime() == 3) {
+                three++;
+            } else if (obj.getTime() == 4) {
+                four++;
+            } else if (obj.getTime() == 5) {
+                five++;
+            } else if (obj.getTime() == 6) {
+                six++;
+            } else if (obj.getTime() == 7) {
+                seven++;
+            } else if (obj.getTime() == 8) {
+                eight++;
+            } else if (obj.getTime() == 9) {
+                nine++;
+            } else if (obj.getTime() == 10) {
+                ten++;
+            } else if (obj.getTime() == 11) {
+                eleven++;
+            } else if (obj.getTime() == 12) {
+                twelve++;
+            } else if (obj.getTime() == 13) {
+                thirteen++;
+            } else if (obj.getTime() == 14) {
+                fourteen++;
+            } else if (obj.getTime() == 15) {
+                fifteen++;
+            } else if (obj.getTime() == 16) {
+                sixteen++;
+            } else if (obj.getTime() == 17) {
+                seventeen++;
+            } else if (obj.getTime() == 18) {
+                eighteen++;
+            } else if (obj.getTime() == 19) {
+                nineteen++;
+            } else if (obj.getTime() == 20) {
+                twenty++;
+            } else if (obj.getTime() == 21) {
+                twentyOne++;
+            } else if (obj.getTime() == 22) {
+                twentyThree++;
+            } else if (obj.getTime() == 23) {
+                twentyThree++;
+            }
+        }
+        JSONObject times = new JSONObject();
+        times.put("zero",zero);
+        times.put("one",one);
+        times.put("two",two);
+        times.put("three",three);
+        times.put("four",four);
+        times.put("five",five);
+        times.put("six",six);
+        times.put("seven",seven);
+        times.put("eight",eight);
+        times.put("nine",nine);
+        times.put("ten",ten);
+
+        times.put("eleven",eleven);
+        times.put("twelve",twelve);
+        times.put("thirteen",thirteen);
+        times.put("fourteen",fourteen);
+        times.put("fifteen",fifteen);
+        times.put("sixteen",sixteen);
+        times.put("seventeen",seventeen);
+        times.put("eighteen",eighteen);
+        times.put("nineteen",nineteen);
+        times.put("twenty",twenty);
+
+        times.put("twentyOne",twentyOne);
+        times.put("twentyTwo",twentyTwo);
+        times.put("twentyThree",twentyThree);
+
+
+        return times;
+    }
 }
