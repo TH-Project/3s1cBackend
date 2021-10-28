4ab2ccb642d62490747afd6a96fcca73bd75b1eb
sungho <leski@naver.com>
2021-10-29 00:24:30 +0900

    adjust retrieveAllRelkeyword's logic

--- src/main/java/com/s1c/rtp/service/RelKeywordService.java
+++ src/main/java/com/s1c/rtp/service/RelKeywordService.java
@@ -38,11 +38,12 @@ public class RelKeywordService {
                 it.remove();;
             }
         }
-
+/*
         for(RelKeywordDto dto : list){
             int mentions = dto.getMentions() / 10;
             dto.setMentions(mentions);
         }
+ */
         return list;
     }
 
